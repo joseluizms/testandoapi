8d0b1869a1b81517f609f2c2161c714395a12185
jluis12369 <jose2021luiz@hotmail.com>
2023-12-12 15:35:10 -0400

    primeiro commit

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,123 +1,66 @@
-import { useContext, useState } from 'react'
-import { ref, uploadBytes, getDownloadURL } from 'firebase/storage'
-
-import styles from './styles.module.css'
-import { storage } from '../../config/firebase'
-import { Auction } from '../../models/Auction'
-import { useNavigate } from 'react-router-dom'
-import { BeatLoader } from 'react-spinners'
-import { SocketContext } from '../../context/SocketContext'
-
-export function formatTime(minutes: number) {
-  const seconds = minutes * 60;
-  const remainingMinutes = Math.floor(seconds / 60);
-  const remainingSeconds = seconds % 60;
-  return `${remainingMinutes.toString().padStart(2, '0')}:${remainingSeconds.toString().padStart(2, '0')}`;
-}
+import React, { useState } from 'react';
+import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import { storage } from '../../config/firebase';
+import { Auction } from '../../models/Auction';
+import styles from './styles.module.css';
 
 const Home = () => {
-  const [image, setImage] = useState<File | null>(null)
-  const [title, setTitle] = useState('')
-  const [description, setDescription] = useState('')
-  const [initialBid, setInitialBid] = useState(1)
-  const [tempMax, setTempMax] = useState(1) // Armazenando minutos
-  const [submitting, isSubmitting] = useState(false)
-
-  const [auctionHistory, setAuctionHistory] = useState<Auction[]>([]); //para pegar historico de leilões
-
-  const navigate = useNavigate()
-  const { socket } = useContext(SocketContext)
-
-  
+  const [image, setImage] = useState<File | null>(null);
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [initialBid, setInitialBid] = useState('');
+  const [tempMax, setTempMax] = useState('');
+  const [submitting, setSubmitting] = useState(false);
+  const navigate = useNavigate();
 
   const startAuction = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    isSubmitting(true)
+    e.preventDefault();
+    if (!title || !description || !initialBid || !tempMax || !image) {
+      alert("Por favor, preencha todos os campos e selecione uma imagem.");
+      return;
+    }
+    setSubmitting(true);
 
-    // Envia a foto do produto para o Storage
-    const uuid = crypto.randomUUID()
-    const storageRef = ref(storage, `products/${uuid}`)
+    const uuid = crypto.randomUUID();
+    const storageRef = ref(storage, `products/${uuid}`);
 
     try {
-      await uploadBytes(storageRef, image as File)
-      const imageURL = await getDownloadURL(storageRef)
-      const auction: Auction = { id: uuid, imageURL, title, description, initialBid, tempMax } // Convertendo minutos para segundos
-      console.log(auction)
+      const imageSnapshot = await uploadBytes(storageRef, image);
+      const imageURL = await getDownloadURL(imageSnapshot.ref);
 
-      socket.emit(`${process.env.REACT_APP_AUCTION_STARTED_EVENT}`, auction)
+      const auctionData: Auction = {
+        id: uuid,
+        imageURL,
+        title,
+        description,
+        initialBid: parseFloat(initialBid),
+        tempMax: parseInt(tempMax) * 60,
+      };
 
-      navigate('/auction', { state: { auction } })
-    } catch (err) {
-      console.log(err)
-      isSubmitting(false)
+      await axios.post('http://localhost:3001/auctions', auctionData);
+      navigate('/auction', { state: { auction: auctionData } });
+    } catch (error) {
+      console.error('Erro ao iniciar o leilão:', error);
+    } finally {
+      setSubmitting(false);
     }
-  }
+  };
 
   return (
     <div className={styles.container}>
-      <div className={styles.auctionArea}>
-        <form
-          className={styles.auctionForm}
-          onSubmit={(e) => startAuction(e)} >
-          <input
-            type="file"
-            required
-            placeholder='Escolha uma foto do produto'
-            accept='image/png,image/jpeg'
-            onChange={(e) => {
-              if (e.target.files && e.target.files.length > 0) {
-                setImage(e.target.files[0])
-              }
-            }} />
-
-          <input
-            required
-            type="text"
-            value={title}
-            placeholder='Título do produto'
-            onChange={(e) => setTitle(e.target.value)} />
-
-          <textarea
-            required
-            placeholder='Descrição do produto'
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}>
-          </textarea>
-
-          <label htmlFor="lanceInicial">
-            Lance inicial (R$)
-          </label>
-
-          <input
-
-            type="number"
-            required
-            placeholder='Lance inicial (R$)'
-            min='1'
-            value={initialBid}
-            onChange={(e) => setInitialBid(+e.target.value)} />
-
-          <label htmlFor="Tempo Maximo de leilão">
-            Tempo Maximo de leilão
-          </label>
-
-          <input
-            type="number"
-            required
-            placeholder={`Tempo Maximo de leilão (${formatTime(tempMax)})`} // Exibindo minutos e segundos no placeholder
-            min='1'
-            value={tempMax}
-            onChange={(e) => setTempMax(+e.target.value)} // Armazenando minutos
-          />
-
-          <p>{formatTime(tempMax)}</p>
-
-          <input type="submit" value="Iniciar" />
-        </form>
-      </div>
-
-      <BeatLoader color='#555' loading={submitting} />
+      <form className={styles.auctionForm} onSubmit={startAuction}>
+        <input type="file" required onChange={e => setImage(e.target.files ? e.target.files[0] : null)} />
+        <input type="text" value={title} onChange={e => setTitle(e.target.value)} required placeholder="Título do Produto" />
+        <textarea value={description} onChange={e => setDescription(e.target.value)} required placeholder="Descrição do Produto" />
+        <input type="number" value={initialBid} onChange={e => setInitialBid(e.target.value)} required placeholder="Lance Inicial (R$)" />
+        <input type="number" value={tempMax} onChange={e => setTempMax(e.target.value)} required placeholder="Tempo Máximo do Leilão (minutos)" />
+        <button type="submit">Iniciar Leilão</button>
+      </form>
+      {submitting && <p>Enviando dados...</p>}
     </div>
-  )
-}
-export default Home
+  );
+};
+
+export default Home;
